80f1e9712ac612b06e69a45aa4f42b942b5618e2
james-things <jharr651@students.kennesaw.edu>
2022-07-14 11:57:49 -0400

    note to self: configure build validation for commits..

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -25,36 +25,36 @@ export class Carousel extends React.Component<CarouselProps, CarouselState> {
                     <div id="1" className="carousel-item relative w-full">
                         <img src="/1.jpg" className="w-full" alt="default"/>
                         <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
-                            <a href="#5" className="btn btn-circle btn-sm">❮</a>
-                            <a href="#2" className="btn btn-circle btn-sm">❯</a>
+                            <a href="#5" className="btn btn-sm btn-circle">❮</a>
+                            <a href="#2" className="btn btn-sm btn-circle">❯</a>
                         </div>
                     </div>
                     <div id="2" className="carousel-item relative w-full">
                         <img src="/2.jpg" className="w-full" alt="default"/>
                         <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
-                            <a href="#1" className="btn btn-circle btn-sm">❮</a>
-                            <a href="#3" className="btn btn-circle btn-sm">❯</a>
+                            <a href="#1" className="btn btn-sm btn-circle">❮</a>
+                            <a href="#3" className="btn btn-sm btn-circle">❯</a>
                         </div>
                     </div>
                     <div id="3" className="carousel-item relative w-full">
                         <img src="/3.jpg" className="w-full" alt="default"/>
                         <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
-                            <a href="#2" className="btn btn-circle btn-sm">❮</a>
-                            <a href="#4" className="btn btn-circle btn-sm">❯</a>
+                            <a href="#2" className="btn btn-sm btn-circle">❮</a>
+                            <a href="#4" className="btn btn-sm btn-circle">❯</a>
                         </div>
                     </div>
                     <div id="4" className="carousel-item relative w-full">
                         <img src="/4.jpg" className="w-full" alt="default"/>
                         <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
-                            <a href="#3" className="btn btn-circle btn-sm">❮</a>
-                            <a href="#5" className="btn btn-circle btn-sm">❯</a>
+                            <a href="#3" className="btn btn-sm btn-circle">❮</a>
+                            <a href="#5" className="btn btn-sm btn-circle">❯</a>
                         </div>
                     </div>
                     <div id="5" className="carousel-item relative w-full">
                         <img src="/5.jpg" className="w-full" alt="default"/>
                         <div className="absolute inset-x-5 top-1/2 flex -translate-y-1/2 justify-between">
-                            <a href="#4" className="btn btn-circle btn-sm">❮</a>
-                            <a href="#1" className="btn btn-circle btn-sm">❯</a>
+                            <a href="#4" className="btn btn-sm btn-circle">❮</a>
+                            <a href="#1" className="btn btn-sm btn-circle">❯</a>
                         </div>
                     </div>
                 </div>
