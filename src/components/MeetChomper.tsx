c74ac72afdbe9df19a6fb357146f2277e2c0b54c
James <jharr651@students.kennesaw.edu>
2023-06-20 12:48:58 -0400

    update meet chomper page content

--- src/components/MeetChomper.tsx
+++ src/components/MeetChomper.tsx
@@ -33,44 +33,38 @@ export class MeetChomper extends React.Component<MeetChomperProps, MeetChomperSt
 
                         <div className="prose p-2 md:col-start-1 md:col-end-2 md:row-start-3 md:row-end-4">
                             <p>
-                                Chomper is a male Leopard Gecko who we believe is about two years old.
-                                We didn&apos;t plan on getting him, but one day we came across someone
-                                who was looking to give him away. He was skinny and had lost most of his coloring,
-                                so we took him in.
+                                Chomper is an adult male Leopard Gecko who we adopted by chance. Chomper&apos;s original
+                                owner was a young child who did not know how to properly care for him, and he was not in the best
+                                health when we originally got him as a result. With time however, he has recovered and become
+                                quite healthy, active, and tame! He is very friendly and enjoys being handled, even choosing
+                                to walk out of his tank on to do so quite frequently.
                             </p>
 
                             <p>
-                                Despite his frail stature, as soon as we put some crickets in his new tank
-                                he leapt into action and ate them one by one with an aggressive &quot;chomp,&quot; and
-                                with that he had his name. Today, Chomper is no longer skinny and unhealthy.
-                                He is now healthy and slowly regaining his coloring over time.
-                            </p>
-
-                            <p>
-                                Initially, Chomper was suffering from Metabolic Bone Disease, which
-                                a common occurrence in Leopard Geckos when they do not get adequate nutrition.
-                                With this disease, the reptile&apos;s bones become softer and weaker, and eventually they
-                                become unable to properly support their own weight, as was the case with Chomper.
+                                We named him Chomper because though he is a very gentle and friendly reptile, he is
+                                quite ferocious when hunting bugs, chomping up as many as he can until he is too full to eat! He
+                                lives on a diet of mostly crickets, with a few other feeder insects mixed in for variety.
+                                He also receives a multivitamin calcium supplement regularly to ensure he gets all the
+                                nutrients he needs.
                             </p>
                         </div>
 
                         <div className="prose p-2 md:col-start-2 md:col-end-3 md:row-start-3 md:row-end-4 md:items-end">
                             <p>
-                                With proper supplementation and well-regulated temperature levels however, he has
-                                almost entirely recovered at this point. He used to be very wobbly and would stand
-                                with his belly on the ground, and now he is almost entirely wobble-free and able to
-                                stand and walk around just fine!
+                                Chomper&apos;s tank is a 40 gallon (long) terrarium, allowing him plenty of space to move around.
+                                He has a 90 degree Fahrenheit hot side and a 75 degree Fahrenheit cool side, allowing him to
+                                thermoregulate as he pleases. His tank is also equipped with a low-power UVB light, which is not strictly
+                                necessary for Leopard Geckos, but is still beneficial for them. The tank&apos;s temperature and humidity
+                                levels are monitored by a custom-built digital thermometer and hygrometer device, and the tank is cleaned
+                                regularly.
                             </p>
 
                             <p>
-                                While Leopard Geckos are not known to be especially intelligent, Chomper still has
-                                a surprising amount of personality and has shown some capacity for simple learning.
-                                He was very shy at first, but with time, he has learned to trust us as his keepers 
-                                and enjoys crawling out of his tank to be handled.
-                            </p>
-
-                            <p>
-                                It really has been quite fascinating learning about reptiles through Chomper!
+                                Though I would not consider myself a reptile person, Chomper has been a great pet and I have enjoyed
+                                caring for him. He is a very interesting animal to watch and interact with (per his own volition),
+                                and I am glad we were able to give him a good home. This website was created as a learning experience,
+                                and also as a way to share Chomper with friends, family, and anyone else who wants to learn about him.
+                                I hope you enjoyed reading about Chomper!
                             </p>
                         </div>
                     </div>
